cbd0cb2c9c56d0334e254c895054c1f8872a1ae6
joshua <joshuasteinerman@gmail.com>
2022-11-07 17:24:54 +0000

    Update 6 files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const mysqlConnection = require("../db/db");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+router.get("/patient", (req, res) => {
+  //mysqlConnection.query("INSERT INTO 'Patient' (createdAt, modifiedAt, email, firstName ) VALUES (now(), now(), 'matt.j.smith@gmail.com', 'Matt')" ,(err, rows, fiels) => {
+    mysqlConnection.query("select * from Patient;" ,(err, rows, fiels) => {  
+    if (!err) {
+      res.json(rows);
+      console.log(fiels);
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 module.exports = router;
