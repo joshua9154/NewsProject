099e69a93ef54782637cc153fc7125d8ce6080c4
joshua <joshuasteinerman@gmail.com>
2022-11-08 16:43:08 +0000

    Update db.js and users.js

--- db/db.js
+++ db/db.js
@@ -1,21 +1,4 @@
 var mysql = require("mysql");
-var mysqlConnection = mysql.createConnection({
-  host: "us-east.connect.psdb.cloud",
-  user: "w5wf94r18cxf2qucnz7a",
-  password: "pscale_pw_UQYzLPCNoNTe4YDn0qjUNWr83tWuwnG8Ckih357oOKT",
-  database: "patient-intake",
-  ssl : {
-      rejectUnauthorized: true
-  }
-});
-
-mysqlConnection.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("SQL Works");
-  }
-});
 
 
 var pool  = mysql.createPool({
--- routes/users.js
+++ routes/users.js
@@ -18,5 +18,19 @@ router.get("/patient", (req, res) => {
     }
   });
 });
-
+router.post("/", (req, res) => {
+  //mysqlConnection.query("INSERT INTO 'Patient' (createdAt, modifiedAt, email, firstName ) VALUES (now(), now(), 'matt.j.smith@gmail.com', 'Matt')" ,(err, rows, fiels) => {
+    const patient= req.body
+   // console.log(patient.email)
+    
+    pool.query("INSERT INTO Patient (id ,createdAt, modifiedAt, email, firstName. lastName, phone, ssn, dateOfBirth, street, city, state, zip, insuranceCompany, plan, groupNumber, medications, surgeries, familyHistory, addictions,questionnaire, signature, middleInitial) VALUES ("+patient.id+",'"+patient.createdAt+"','"+ patient.modifiedAt+"','" +patient.email+"',' "+patient.firstName+"','"+patient.lastName+"','"+patient.phone+"','"+patient.ssn+"','"+patient.dateOfBirth+"','"+patient.street+"','"+patient.city+"','"+patient.state+"','"+patient.zip+"','"+patient.insuranceCompany+"','"+patient.plan+"','"+patient.groupNumber+"','"+patient.medications+"','"+patient.surgeries+"','"+patient.familyHistory+"','"+patient.addictions+"','"+patient.questionnaire+"','"+patient.signature+"','"+patient.middleInitial+"');" ,(err, rows, fiels) => {  
+    if (!err) {
+      res.json(rows);
+      console.log(fiels);
+    } else {
+      console.log(err);
+    }
+  });
+  res.status(201).send(err)
+});
 module.exports = router;
