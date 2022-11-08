08fa4e9e64891e2e42d678a3439652ee18af1704
joshua <joshuasteinerman@gmail.com>
2022-11-08 15:06:48 +0000

    Update db.js and users.js

--- db/db.js
+++ db/db.js
@@ -17,4 +17,17 @@ mysqlConnection.connect((err) => {
   }
 });
 
-module.exports = mysqlConnection;
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'us-east.connect.psdb.cloud',
+  user            : 'w5wf94r18cxf2qucnz7a',
+  password        : 'pscale_pw_UQYzLPCNoNTe4YDn0qjUNWr83tWuwnG8Ckih357oOKT',
+  database        : 'patient-intake',
+  ssl : {
+      rejectUnauthorized: true
+  }
+  
+});
+
+module.exports = pool;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const mysqlConnection = require("../db/db");
+const pool = require("../db/db");
+
 
 router.get("/", (req, res) => {
   res.json("Hello");
@@ -8,7 +9,7 @@ router.get("/", (req, res) => {
 
 router.get("/patient", (req, res) => {
   //mysqlConnection.query("INSERT INTO 'Patient' (createdAt, modifiedAt, email, firstName ) VALUES (now(), now(), 'matt.j.smith@gmail.com', 'Matt')" ,(err, rows, fiels) => {
-    mysqlConnection.query("select * from Patient;" ,(err, rows, fiels) => {  
+    pool.query("select * from Patient;" ,(err, rows, fiels) => {  
     if (!err) {
       res.json(rows);
       console.log(fiels);
