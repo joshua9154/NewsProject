c8824fb354ca3fe799ea7e2add61a5fdb8eeab90
joshua <joshuasteinerman@gmail.com>
2022-11-08 20:03:23 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -23,8 +23,9 @@ router.post("/", (req, res) => {
   //mysqlConnection.query("INSERT INTO 'Patient' (createdAt, modifiedAt, email, firstName ) VALUES (now(), now(), 'matt.j.smith@gmail.com', 'Matt')" ,(err, rows, fiels) => {
     const patient= req.body
    // console.log(patient.email)
-    
-    pool.query("INSERT INTO Patient (id ,modifiedAt,email, firstName, lastName, phone, ssn,dateOfBirth, street, city, state, zip, insuranceCompany, plan, groupNumber, medications, surgeries, familyHistory, addictions,questionnaire, signature, ,middileInitial) VALUES ("+patient.id+",'1971-01-01 00:00:01','" +patient.email+"',' "+patient.firstName+"','"+patient.lastName+"','"+patient.phone+"','"+patient.ssn+"','1971-01-01 00:00:01','"+patient.street+"','"+patient.city+"','"+patient.state+"','"+patient.zip+"','"+patient.insuranceCompany+"','"+patient.plan+"','"+patient.groupNumber+"','["+patient.medications+"]','["+patient.surgeries+"]','["+patient.familyHistory+"]','["+patient.addictions+"]','["+patient.questionnaire+"]','"+patient.signature+"','"+patient.middleInitial+"');" ,(err, rows, fiels) => {  
+ 
+    //pool.query("INSERT INTO Patient (createdAt, modifiedAt, email, firstName ) VALUES  ('2022-11-07T17:33:19.885Z','2022-11-07T17:33:19.000Z','" +patient.email+"','"+patient.firstName+"','"+patient.lastName+"','"+patient.phone+"','"+patient.ssn+"','1971-01-01 00:00:01','"+patient.street+"','"+patient.city+"','"+patient.state+"','"+patient.zip+"','"+patient.insuranceCompany+"','"+patient.plan+"','"+patient.groupNumber+"','"+patient.medications+"','"+patient.surgeries+"','"+patient.familyHistory+"','"+patient.addictions+"','"+patient.questionnaire+"','"+patient.signature+"','"+patient.middleInitial+"');" ,(err, rows, fiels) => {  
+    pool.query("INSERT INTO Patient (createdAt, modifiedAt, email, firstName ) VALUES  (\""+ Date.now()+"\",\"2022-11-07T17:33:19.000Z\",\"" +patient.email+"\",\""+patient.firstName+"\");" ,(err, rows, fiels) => {  
     if (!err) {
       res.json(rows);
       console.log(fiels);
@@ -32,6 +33,32 @@ router.post("/", (req, res) => {
       console.log(err);
     }
   });
-  res.status(201).send(patient.id)
+     res.status(201).send("hi")
+  
+});
+router.post("/contact", (req, res) => {
+   const contact= req.body
+     
+    pool.query("INSERT INTO Contact (modifiedAt,patientId,firstName,lastName,phone,email,dateOfBirth,street,city,state,zip,relationToPatient,type,emergencyPriority,signature) VALUES ('2007-05-08 12:35:29.123',"+contact.patientId+",'"+contact.firstName+"','"+contact.lastName+"','"+contact.phone+"','"+contact.email+"','2007-05-08 12:35:29.123','"+contact.street+"','"+contact.city+"','"+contact.state+"','"+contact.zip+"','"+contact.relationToPatient+"','"+contact.type+"','"+contact.emergencyPriority+"','"+contact.signature+"');" ,(err, rows, fiels) => {  
+    if (!err) {
+      res.json(rows);
+      console.log(fiels);
+    } else {
+      console.log(err);
+    }
+  });
+     
+ res.status(201).send("Contact "+contact.firstName+" "+ contact.lastName +" has been added to the contact list for patient "+contact.patientId) 
+});
+
+router.get("/contact", (req, res) => {
+   pool.query("select * from Contact;" ,(err, rows, fiels) => {  
+    if (!err) {
+      res.json(rows);
+      console.log(fiels);
+    } else {
+      console.log(err);
+    }
+  });
 });
 module.exports = router;
