4aff0cf448317dfff5224191d6df54eaec46b517
joshua <joshuasteinerman@gmail.com>
2022-11-21 20:04:44 +0000

    Update contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,7 +5,7 @@ const pool = require("../db/db");
 
 router.post("/", (req, res) => {
    var contact= req.body
-     
+       if (validateContact(contact)=="ok"){
     pool.query("INSERT INTO Contacts (patientId,title,firstName,middleInitial,lastName,phone,email,sex,dateOfBirth,street,city,state,zip,relationToPatient,type,emergencyPriority,signature) VALUES ('"+contact.patientId+"','"+contact.title+"','"+contact.firstName+"','"+contact.middleInitial+"','"+contact.lastName+"','"+contact.phone+"','"+contact.email+"','"+contact.sex+"','"+contact.dateOfBirth+"','"+contact.street+"','"+contact.city+"','"+contact.state+"','"+contact.zip+"','"+contact.relationToPatient+"','"+contact.type+"','"+contact.emergencyPriority+"','"+contact.signature+"');" ,(err, rows, fiels) => {  
     if (!err) {
       res.json(rows);
@@ -15,11 +15,15 @@ router.post("/", (req, res) => {
       console.log(err);
     }
   });
+    }else{
+         res.status(400).send(validateContact(contact))
+     }
      
 // res.status(201).send("Contact "+contact.firstName+" "+ contact.lastName +" has been added to the contact list for contact "+contact.patientId) 
 });
 router.put("/", (req, res) => {
     var contact= req.body
+     if (validateContact(contact)=="ok"){
    //   pool.query("INSERT INTO Patient (modifiedAt,email,firstName,lastName,phone,ssn,dateOfBirth,street,city,state,zip,insuranceCompany,plan,groupNumber,medications,surgeries,familyHistory,addictions,questionnaire,signature,middleInitial) VALUES ('2007-05-08 12:35:29.123','"+contact.email+"','"+contact.firstName+"','"+contact.lastName+"','"+contact.phone+"','"+contact.ssn+"','"+contact.dateOfBirth+"','"+contact.street+"','"+contact.city+"','"+contact.state+"','"+contact.zip+"','"+contact.insuranceCompany+"','"+contact.plan+"','"+contact.groupNumber+"','"+contact.medications+"','"+contact.surgeries+"','"+contact.familyHistory+"','"+contact.addictions+"','"+contact.questionnaire+"','"+contact.signature+"','"+contact.middleInitial+"');" ,(err, rows, fiels) => {  
        pool.query("UPDATE Contacts SET patientId ='"+ contact.patientId+"',title='"+contact.title+"',firstName='"+contact.firstName+"',middleInitial='"+contact.middleInitial+"',lastName='"+contact.lastName+"',phone='"+contact.phone+"',email='"+contact.email+"',sex='"+contact.sex+"',dateOfBirth='"+contact.dateOfBirth+"',street='"+contact.street+"',city='"+contact.city+"',state='"+contact.state+"',zip='"+contact.zip+"',relationToPatient='"+contact.relationToPatient+"',type='"+contact.type+"',emergencyPriority='"+contact.emergencyPriority+"',patientId='"+contact.patientId+"',signature='"+contact.signature+"' WHERE contactId = '"+contact.contactId+"';"  ,(err, rows, fiels) => {  
 
@@ -32,7 +36,9 @@ router.put("/", (req, res) => {
     }
   });
    //  res.status(201).send("Patient "+contact.firstName+" "+contact.lastName+" has beed added to the contact list.")
-  
+     }else{
+         res.status(400).send(validateContact(contact))
+     }
 });
 
 
@@ -93,5 +99,33 @@ router.delete('/:id',(req,res,next)=> {
     }
   });
   });
+  function validateContact(contact) {
+  result="ok"
+  if(validateTitle(contact.title)){
+    return "Please use titles Mr, Mrs, Ms or Miss not "+ contact.title+"."
+  }
+  return result
+}
+
+function validateTitle(title) {
+   input= title.toLowerCase();
+    if(input==""){
+     return false
+   }
+   if(input=="mr"){
+     return false
+   }
+   if(input=="mrs"){
+     return false
+   }
+   if(input=="ms"){
+     return false
+   }
+   if(input=="miss"){
+     return false
+   }
+  return true
+}
+
   
 module.exports = router;
