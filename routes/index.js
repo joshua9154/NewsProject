fa709ab9500be762206dc91f4187c0e7654a53a6
joshua <joshuasteinerman@gmail.com>
2022-11-25 20:07:01 +0000

    Update index.js and patientPage.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
  var express = require("express");
  var router = express.Router();
- //const async = require("hbs/lib/async");
  const pool = require("../db/db");
 
-// /* GET home page. */
- //router.get("/", function(req, res, next) {
- //  res.render("index", { title: "Express" });
-// });
-
- //router.post("/subscribe", function(req, res, next) {
-  // const { name, email } = req.body;
 router.get("/", function(req, res, next) {
    res.render("patientPage", { title: "Patient Intake" });
  });
  
+ 
+ 
+ 
  router.post("/Contact", async function(req, res, next) {
-   
+    const { patientId, title, firstName, middleInitial, lastName, phone, email, sex, dob, street, city, state, zip, relationToPatient, type, emergencyPriority, signature } = req.body;
+    var result=1
+    var insert = ""+patientId+"','"+title+"','"+firstName+"','"+middleInitial+"','"+lastName+"','"+phone+"','"+email+"','"+sex+"','"+dob+"','"+street+"','"+city+"','"+state+"','"+zip+"','"+relationToPatient+"','"+type+"','"+emergencyPriority+"','"+signature+""
+    
+    var re='[]'
+    while (re=='[]'){
+        re= await insertContacts(insert)
+    }
+    result=re
+    
     res.render("subscribed", {
-    title: "You are subscribed",
+    title: "Contact",
     result 
     
   });
@@ -26,11 +30,14 @@ router.get("/", function(req, res, next) {
  router.post("/Subscribe", async function(req, res, next) {
    const { title, firstName, middleInitial, lastName, phone, email, sex, ssn, dob, street, city, state, zip, insuranceCompany, plan, groupNumber, cardHolder, signature } = req.body;
    var result= 0
-  
-   // 1. Validate the user data
-  // 2. Subscribe the user to the mailing list
-   // 3. Send a confirmation email
-       pool.query("INSERT INTO Patients (title,firstName,middleInitial,lastName,phone,email,sex,ssn,dateOfBirth,street,city,state,zip,insuranceCompany,plan,groupNumber,cardholder,signature) VALUES ('"+title+"','"+firstName+"','"+middleInitial+"','"+lastName+"','"+phone+"','"+email+"','"+sex+"','"+ssn+"','"+dob+"','"+street+"','"+city+"','"+state+"','"+zip+"','"+insuranceCompany+"','"+plan+"','"+groupNumber+"','"+cardHolder+"','"+signature+"');" ,(err, rows, fiels) => {  
+   var insert = ""+title+"','"+firstName+"','"+middleInitial+"','"+lastName+"','"+phone+"','"+email+"','"+sex+"','"+ssn+"','"+dob+"','"+street+"','"+city+"','"+state+"','"+zip+"','"+insuranceCompany+"','"+plan+"','"+groupNumber+"','"+cardHolder+"','"+signature+""
+   var re='[]'
+    while (re=='[]'){
+        re= await insertPatients(insert)
+    }
+    result=re
+   
+    /*   pool.query("INSERT INTO Patients (title,firstName,middleInitial,lastName,phone,email,sex,ssn,dateOfBirth,street,city,state,zip,insuranceCompany,plan,groupNumber,cardholder,signature) VALUES ('"+title+"','"+firstName+"','"+middleInitial+"','"+lastName+"','"+phone+"','"+email+"','"+sex+"','"+ssn+"','"+dob+"','"+street+"','"+city+"','"+state+"','"+zip+"','"+insuranceCompany+"','"+plan+"','"+groupNumber+"','"+cardHolder+"','"+signature+"');" ,(err, rows, fiels) => {  
    
     if (!err) {
       console.log(rows);
@@ -41,28 +48,19 @@ router.get("/", function(req, res, next) {
     }
   });
   
-     /*re= await emails(email)
-     console.log(re)
-    if (re!='[]'){
-      result=re;
-      
-    }*/
+    
      var re='[]'
     while (re=='[]'){
         re= await emails(email)
     }
     result=re
-    
+    */
     res.render("subscribed", {
     title: "You are subscribed",
     result 
     
   });
-  //res.render("subscribed", {
-   // title: "You are subscribed",
-   // result 
-  //  
-  //});
+
  });
  
  async function emails(email){
@@ -73,12 +71,68 @@ router.get("/", function(req, res, next) {
     
      if (!err) {
      result= JSON.stringify(rows)
-    // if  (res[3]==undefined){
+ 
          console.log(result)
          resolve(result)
-    //     }else
+  
          }
     else{
+           resolve(err)
+        }
+         });
+ 
+  });
+    
+       rest =await myPromise;
+       console.log(rest)
+       return rest;
+}
+async function insertPatients(insert){
+   
+  let myPromise = new Promise(function(resolve, reject) {
+    
+    pool.query("INSERT INTO Patients (title,firstName,middleInitial,lastName,phone,email,sex,ssn,dateOfBirth,street,city,state,zip,insuranceCompany,plan,groupNumber,cardholder,signature) VALUES ('"+insert+"');",(err, rows, fiels) => {  
+    
+     if (!err) {
+        //result= JSON.stringify(rows)
+       //  console.log(result)
+         console.log(rows.insertId)
+         resolve(rows.insertId)
+  
+         }
+    else{
+           console.log(rows)
+           console.log(fiels)
+           console.log(err)
+           resolve(err)
+        }
+         });
+ 
+  });
+    
+       rest =await myPromise;
+       console.log(rest)
+       return rest;
+}
+
+
+ async function insertContacts(insert){
+   
+  let myPromise = new Promise(function(resolve, reject) {
+    
+    pool.query("INSERT INTO Contacts (patientId, title,firstName,middleInitial,lastName,phone,email,sex,dateOfBirth,street,city,state,zip,relationToPatient,type,emergencyPriority,signature) VALUES ('"+insert+"');",(err, rows, fiels) => {  
+    
+     if (!err) {
+     result= JSON.stringify(rows)
+         console.log(result)
+         console.log(rows.insertId)
+         resolve(rows.insertId)
+  
+         }
+    else{
+           console.log(rows.insertId)
+           console.log(fiels)
+           console.log(err)
            resolve(err)
         }
          });
