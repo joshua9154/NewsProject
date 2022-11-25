eb9a2cdffb86ec3e9964e89ce1d172b27fda9144
joshua <joshuasteinerman@gmail.com>
2022-11-25 18:24:26 +0000

    Update index.js and patientPage.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
  var express = require("express");
  var router = express.Router();
+ //const async = require("hbs/lib/async");
  const pool = require("../db/db");
 
 // /* GET home page. */
@@ -12,24 +13,28 @@
 router.get("/", function(req, res, next) {
    res.render("patientPage", { title: "Patient Intake" });
  });
+ 
 
- router.post("/Subscribe", function(req, res, next) {
+ router.post("/Subscribe", async function(req, res, next) {
    const { title, firstName, middleInitial, lastName, phone, email, sex, ssn, dob, street, city, state, zip, insuranceCompany, plan, groupNumber, cardHolder, signature } = req.body;
    var result=0
    // 1. Validate the user data
   // 2. Subscribe the user to the mailing list
    // 3. Send a confirmation email
        pool.query("INSERT INTO Patients (title,firstName,middleInitial,lastName,phone,email,sex,ssn,dateOfBirth,street,city,state,zip,insuranceCompany,plan,groupNumber,cardholder,signature) VALUES ('"+title+"','"+firstName+"','"+middleInitial+"','"+lastName+"','"+phone+"','"+email+"','"+sex+"','"+ssn+"','"+dob+"','"+street+"','"+city+"','"+state+"','"+zip+"','"+insuranceCompany+"','"+plan+"','"+groupNumber+"','"+cardHolder+"','"+signature+"');" ,(err, rows, fiels) => {  
-    
+   
     if (!err) {
-      
-       result=rows.insertId[0];
       console.log(fiels);
     } else {
-       result=1
+       
       console.log(err);
     }
   });
+  
+    re= await emails(email)
+    if (re!=[]){
+      result=re;
+    }
 
  res.render("subscribed", {
     title: "You are subscribed",
@@ -42,5 +47,30 @@ router.get("/", function(req, res, next) {
   //  
   //});
  });
+ 
+ async function emails(email){
+   
+  let myPromise = new Promise(function(resolve, reject) {
+    
+    pool.query("select id From Patients Where email ='"+email+"';",(err, rows, fiels) => {  
+    
+     if (!err) {
+     result= JSON.stringify(rows)
+    // if  (res[3]==undefined){
+         console.log(result)
+         resolve(result)
+    //     }else
+         }
+    else{
+           resolve(err)
+        }
+         });
+ 
+  });
+    
+       rest =await myPromise;
+       console.log(rest)
+       return rest;
+}
 
 module.exports = router;
